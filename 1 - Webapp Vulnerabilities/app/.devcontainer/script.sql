6c0907c8d9148319b7a8791d29de51ede46d3db8
niyolu <nijolu@live.de>
2022-11-23 22:09:26 +0100

    fixed docker

--- 1 - Webapp Vulnerabilities/app/.devcontainer/script.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS ponyfarm;
-USE ponyfarm;
-CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), password VARCHAR(255), name VARCHAR(255));
-CREATE TABLE IF NOT EXISTS posts(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), text VARCHAR(999));
-INSERT INTO users (email, password, name) VALUES ('admin@example.com', 'password', 'Admin');
-INSERT INTO posts (username, text) VALUES ('Admin', 'Welcome to <blog name>, please behave and stuff');
