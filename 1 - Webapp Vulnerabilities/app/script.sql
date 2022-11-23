e82ce582b4a88f8c4e2538ef91430e304b614dfb
niyolu <nijolu@live.de>
2022-11-23 22:09:07 +0100

    moved script, added checks for tables

---
+++ 1 - Webapp Vulnerabilities/app/script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS ponyfarm;
+USE ponyfarm;
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), password VARCHAR(255), name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS posts(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), text VARCHAR(999));
+
+IF NOT EXISTS (SELECT * from users WHERE email = 'admin@example.com' AND username = 'Admin' AND password = 'password')
+    INSERT INTO users (email, password, name) VALUES ('admin@example.com', 'password', 'Admin');
+
+
+IF NOT EXISTS (SELECT * from posts WHERE username = 'Admin' AND text = 'Welcome to <blog name>, please behave and stuff')
+    INSERT INTO posts (username, text) VALUES ('Admin', 'Welcome to <blog name>, please behave and stuff');
