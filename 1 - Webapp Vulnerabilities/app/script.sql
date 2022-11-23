45a342cc1409acfb4fb140296c602d333e68ecb4
niyolu <nijolu@live.de>
2022-11-23 22:53:24 +0100

    added duplicate checking on db init script

--- 1 - Webapp Vulnerabilities/app/script.sql
+++ 1 - Webapp Vulnerabilities/app/script.sql
@@ -1,11 +1,14 @@
 CREATE DATABASE IF NOT EXISTS ponyfarm;
 USE ponyfarm;
-CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), password VARCHAR(255), name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) UNIQUE, password VARCHAR(255), name VARCHAR(255) UNIQUE);
 CREATE TABLE IF NOT EXISTS posts(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), text VARCHAR(999));
 
-IF NOT EXISTS (SELECT * from users WHERE email = 'admin@example.com' AND username = 'Admin' AND password = 'password')
-    INSERT INTO users (email, password, name) VALUES ('admin@example.com', 'password', 'Admin');
+INSERT INTO users (email, password, name)
+SELECT 'admin@example.com', 'password', 'Admin' FROM DUAL
+WHERE NOT EXISTS (SELECT * from users
+    WHERE email = 'admin@example.com' AND name = 'Admin' AND password = 'password' LIMIT 1);
 
-
-IF NOT EXISTS (SELECT * from posts WHERE username = 'Admin' AND text = 'Welcome to <blog name>, please behave and stuff')
-    INSERT INTO posts (username, text) VALUES ('Admin', 'Welcome to <blog name>, please behave and stuff');
+INSERT INTO posts (username, text)
+SELECT 'Admin',  'Welcome to <blog name>, please behave and stuff' FROM DUAL
+WHERE NOT EXISTS (SELECT * from posts
+    WHERE username = 'Admin' AND text = 'Welcome to <blog name>, please behave and stuff' LIMIT 1);
