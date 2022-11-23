8b9c4df7fc3f56ac783c1e31840fd387461e64fc
MarcelMarek <114317273+MarcelMarek@users.noreply.github.com>
2022-11-23 22:17:45 +0100

    adding postsRouter

--- 1 - Webapp Vulnerabilities/app/src/routes/posts.js
+++ 1 - Webapp Vulnerabilities/app/src/routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const router = express.Router();
+const postsRouter = express.Router();
 const db = require('../helpers/db');
 const path = require('path');
 
-router.get('/', (req, res) => {
-    const static_file = path.join(__dirname, "../../", "static/post.html");
+postsRouter.get('/', (req, res) => {
+    const static_file = path.join(__dirname, "../../", "static/posts.html");
     // get posts
     // insert posts into static file
     res.sendFile(static_file);
 });
 
+module.exports = postsRouter;
