3ba5a3e505b4ba242e083d254bb551080e2ed50c
niyolu <nijolu@live.de>
2022-11-24 00:50:45 +0100

    added registration

--- 1 - Webapp Vulnerabilities/app/src/app.js
+++ 1 - Webapp Vulnerabilities/app/src/app.js
@@ -27,6 +27,7 @@ app.use(cookieSession({
     maxAge: 1000 * 60 * 60,
     secure: false
   },
+  keys: []
 }))
 app.use(express.static(path.join(__dirname, 'public')));
 
--- 1 - Webapp Vulnerabilities/app/src/routes/login.js
+++ 1 - Webapp Vulnerabilities/app/src/routes/login.js
@@ -23,12 +23,34 @@ router.post('/', (req, res) => {
     const user = rows[0];
     
     req.session.isAuth = valid;
-    
+
     console.log('rows', rows)
     
     // add session logic
     res.send((valid ? `welcome user ${user.name}` : 'bad authentification') + "\n");
   });
+});
+
+router.post('/register', (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  const user_exists_query = `SELECT * FROM users WHERE name = ${username}`;
+  db.connection.query(user_exists_query, (err, rows, fields) => {
+    if (err || rows.length > 0) {
+      res.status(500).send("Failed to register");
+    };
+  });
+
+  const insert_user_query = `INSERT INTO users (email, password, name) VALUES ('${email}', '${password}', '${username}')`;
+  db.connection.query(user_exists_query, (err, rows, fields) => {
+    if (err) {
+      res.status(500).send("Failed to register");
+    };
+  });
+
+  req.session.isAuth = valid;
 });
 
 module.exports = router;
