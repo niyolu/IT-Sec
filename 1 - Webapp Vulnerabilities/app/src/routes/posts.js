8284e6018ea6d3e5ec86ae7fd3d1d354d1dd1a81
MarcelMarek <114317273+MarcelMarek@users.noreply.github.com>
2022-11-23 23:00:49 +0100

    add post

--- 1 - Webapp Vulnerabilities/app/src/routes/posts.js
+++ 1 - Webapp Vulnerabilities/app/src/routes/posts.js
@@ -2,12 +2,27 @@ const express = require('express');
 const postsRouter = express.Router();
 const db = require('../helpers/db');
 const path = require('path');
+const app = require('../app');
 
 postsRouter.get('/', (req, res) => {
     const static_file = path.join(__dirname, "../../", "static/posts.html");
     // get posts
+
+    
     // insert posts into static file
+
     res.sendFile(static_file);
 });
+
+postsRouter.post("/post", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+    .then(item => {
+        console.log(item);
+    })
+    .catch(err => {
+        res.status(400).send("unable to save to database");
+    });
+   });
 
 module.exports = postsRouter;
