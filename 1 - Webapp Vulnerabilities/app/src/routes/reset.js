ea1bb5f621b260c6924d37e48297a5310bf9898d
MarcelMarek <114317273+MarcelMarek@users.noreply.github.com>
2022-11-25 20:04:58 +0100

    added easier handling of routes

--- 1 - Webapp Vulnerabilities/app/src/routes/reset.js
+++ 1 - Webapp Vulnerabilities/app/src/routes/reset.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../helpers/db');
 const { get_user } = require('../helpers/sessionstore');
+const path = require('path');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../../", "static/reset.html"));
+});
 
 
 router.get('/', (req, res) => {
